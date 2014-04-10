d2377ec307748d377f6531444207a147d18dad51
kumar venkatesh <kumarv@motorola.com>
2014-04-10 19:05:31 +0530

    power: qpnp-charger: Charging doesn't resume at soc 99
    The battery status stays at FULL even though SOC drops to or below
    99 from 100 with charger connected, as charging doesn't resume.
    The charging would resume on re-insertion of USB Accessory
    
    This change makes sure that the charging resumes when SOC drops to
    or below 99 from 100, when charger is connected.
    
    Change-Id: I524e90655292c4057a270466c38b5670da68d58b
    Signed-off-by: kumar venkatesh <kumarv@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/625682
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    (cherry picked from commit 0caa2dca70b9e5f09381ee34a29d32868d781510)

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -2470,7 +2470,6 @@ get_prop_capacity(struct qpnp_chg_chip *chip)
 				&& charger_in
 				&& !chip->bat_is_cool
 				&& !chip->bat_is_warm
-				&& !chip->resuming_charging
 				&& !chip->charging_disabled
 				&& chip->soc_resume_limit
 				&& soc <= chip->soc_resume_limit) {
@@ -2478,6 +2477,7 @@ get_prop_capacity(struct qpnp_chg_chip *chip)
 			chip->resuming_charging = true;
 			qpnp_chg_irq_wake_enable(&chip->chg_fastchg);
 			qpnp_chg_set_appropriate_vbatdet(chip);
+			chip->resuming_charging = false;
 			qpnp_chg_charge_en(chip, !chip->charging_disabled);
 		}
 		if (soc == 0) {
