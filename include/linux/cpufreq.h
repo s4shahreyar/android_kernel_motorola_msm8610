87e7a4955e340429662e12f2b649a4e8f1a2f6a5
Paul Reioux <reioux@gmail.com>
2014-11-25 21:10:52 +0530

    Implement Intellidemand governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -379,6 +379,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALPHA)
 extern struct cpufreq_governor cpufreq_gov_alpha;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alpha)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
 #endif
 
 
