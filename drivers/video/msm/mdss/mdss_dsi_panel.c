71c423bc8bffbe6e9d6c1a95da94179bbe85dedc
goutamniwas <goutamniwas@gmail.com>
2014-12-07 21:05:09 +0530

    t2u : fix screen freeze after wakeup during call

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -600,6 +600,7 @@ static int mdss_dsi_panel_cont_splash_on(struct mdss_panel_data *pdata)
 	extern bool t2u_scr_suspended;
 	extern void touch_suspend(void);
 	extern void touch_resume(void);
+	extern bool prox_covered;
 #endif
 
 
@@ -715,7 +716,8 @@ disable_regs:
 #endif
 #ifdef CONFIG_TOUCHSCREEN_TAP2UNLOCK
 	t2u_scr_suspended = true;
-	t2u_allow = false;
+	if(!prox_covered)
+		t2u_allow = false;
 	if (t2u_switch > 0)
 		touch_resume();
 #endif
