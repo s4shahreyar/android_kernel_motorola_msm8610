b016699bd30a441961d1e206d493bc7ad8acbf22
goutamniwas <goutamniwas@gmail.com>
2014-12-20 21:32:26 +0530

    t2u/dt2w : Improve dt2w detection and disable it when t2u is on

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -34,6 +34,7 @@
 #endif
 #include <linux/hrtimer.h>
 #include <asm-generic/cputime.h>
+#include <linux/input/tap2unlock.h>
 
 /* uncomment since no touchscreen defines android touch, do that here */
 //#define ANDROID_TOUCH_DECLARED
@@ -152,25 +153,22 @@ static void detect_doubletap2wake(int x, int y, bool st)
 		touch_cnt = false;
 		if (touch_nr == 0) {
 			new_touch(x, y);
-		} else if (touch_nr == 1) {
+		} else {
 			if ((calc_feather(x, x_pre) < DT2W_FEATHER) &&
 			    (calc_feather(y, y_pre) < DT2W_FEATHER) &&
-			    ((ktime_to_ms(ktime_get())-tap_time_pre) < DT2W_TIME))
-				touch_nr++;
+			    ((ktime_to_ms(ktime_get())-tap_time_pre) < DT2W_TIME)) {
+				pr_info(LOGTAG"ON\n");
+				exec_count = false;
+				doubletap2wake_pwrtrigger();
+				doubletap2wake_reset();
+			}
+				
 			else {
 				doubletap2wake_reset();
 				new_touch(x, y);
 			}
-		} else {
-			doubletap2wake_reset();
-			new_touch(x, y);
-		}
-		if ((touch_nr > 1)) {
-			pr_info(LOGTAG"ON\n");
-			exec_count = false;
-			doubletap2wake_pwrtrigger();
-			doubletap2wake_reset();
-		}
+		} 
+		
 	}
 }
 
@@ -190,7 +188,10 @@ static void dt2w_input_event(struct input_handle *handle, unsigned int type,
 		(code==ABS_MT_TRACKING_ID) ? "ID" :
 		"undef"), code, value);
 #endif
-	if (!dt2w_scr_suspended)
+	if (!dt2w_scr_suspended )
+		return;
+
+	if (t2u_switch > 0)
 		return;
 
 	if (code == ABS_MT_SLOT) {
