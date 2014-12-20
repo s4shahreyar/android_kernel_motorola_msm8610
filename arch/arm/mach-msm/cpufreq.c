7130288216175b092bc126005f14d24349d47a61
goutamniwas <goutamniwas@gmail.com>
2014-12-20 21:34:54 +0530

    Try to fix intellidemand

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -389,6 +389,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	return 0;
 }
 
+
 static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
 {
