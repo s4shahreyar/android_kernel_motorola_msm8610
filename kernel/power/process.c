9b0697ea2891356c95d252b66d3ec1ea7fb24599
goutamniwas <goutamniwas@gmail.com>
2014-12-28 21:05:48 +0530

    Suspend: enable freeze timeout configuration through sys At present, the value of timeout for freezing is 20s, which is meaningless in case that one thread is frozen with mutex locked and another thread is trying to lock the mutex, as this time of freezing will fail unavoidably. And if there is no new wakeup event registered, the system will waste at most 20s for such meaningless trying of freezing.
    With this patch, the value of timeout can be configured to smaller
    value, so such meaningless trying of freezing will be aborted in
    earlier time, and later freezing can be also triggered in earlier
    time. And more power will be saved.
    In normal case on mobile phone, it costs real little time to freeze
    processes. On some platform, it only costs about 20ms to freeze
    user space processes and 10ms to freeze kernel freezable threads.
    
    [Blechd0se] lowering the timeout to 5s, which seems to have a faster
    deepsleep state. I didn't encounter any problems doing so.
    
    Signed-off-by: Liu Chuansheng <chuansheng.liu@intel.com>
    Signed-off-by: Li Fei <fei.li@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>
    
    Conflicts:
    	kernel/power/main.c
    
    idea is taken here - Blechd0se/kernel-moto-g@8de7c68

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -23,7 +23,6 @@
 /* 
  * Timeout for stopping processes
  */
-#define TIMEOUT	(20 * HZ)
 
 static int try_to_freeze_tasks(bool user_only)
 {
@@ -38,7 +37,7 @@ static int try_to_freeze_tasks(bool user_only)
 
 	do_gettimeofday(&start);
 
-	end_time = jiffies + TIMEOUT;
+	end_time = jiffies + msecs_to_jiffies(freeze_timeout_msecs);
 
 	if (!user_only)
 		freeze_workqueues_begin();
